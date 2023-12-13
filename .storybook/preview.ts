9d0417fda6c8e5eab9239fcfa2669f146e22cbc0
Caroline Lopes <kaka201155@gmail.com>
2023-12-13 14:19:38 -0300

    feat: Create project System Design

---
+++ .storybook/preview.ts
@@ -0,0 +1,21 @@
+import type { Preview } from "@storybook/react";
+import { themes } from '@storybook/theming';
+
+import '../src/styles/global.css';
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+      docs: {
+        theme: themes.dark,
+      },
+    },
+  },
+};
+
+export default preview;
